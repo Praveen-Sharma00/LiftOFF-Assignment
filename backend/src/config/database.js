030f717963cd7bbf9842fae2bc8cc547c9f26536
Praveen-Sharma00 <52413182+Praveen-Sharma00@users.noreply.github.com>
2020-10-03 18:29:06 +0530

    Fixed PORT bug in dbConfig

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,38 +1,38 @@
-export default function getDbConfig(){
-  let dbUrl='';
+export default function getDbConfig() {
+  let dbUrl = '';
 
-  const dbConfig={
-    port:process.env.DB_PORT,
+  const dbConfig = {
+    port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     name: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-  }
+  };
 
-  for(let key of Object.keys(dbConfig)){
-    if(dbConfig[key]==='undefined'){
-      delete dbConfig[key]
+  for (let key of Object.keys(dbConfig)) {
+    if (dbConfig[key] === 'undefined') {
+      delete dbConfig[key];
     }
   }
 
-  if(process.env.NODE_ENV==='development'){
-    dbUrl += `mongodb://`
-  } else if (process.env.NODE_ENV === 'production'){
-    dbUrl += `mongodb+srv://`
+  if (process.env.NODE_ENV === 'development') {
+    dbUrl += `mongodb://`;
+  } else if (process.env.NODE_ENV === 'production') {
+    dbUrl += `mongodb+srv://`;
   }
 
-  if(dbConfig['user'] && dbConfig['password']){
-    dbUrl += `${process.env.DB_USER}:${process.env.DB_PASSWORD}`
+  if (dbConfig['user'] && dbConfig['password']) {
+    dbUrl += `${process.env.DB_USER}:${process.env.DB_PASSWORD}@`;
   }
 
-  dbUrl += `@${process.env.DB_HOST}`
+  dbUrl += `${process.env.DB_HOST}`;
 
-  if(dbConfig['port']){
-    dbUrl += `:${process.env.DB_PORT}`
+  if (dbConfig['port']) {
+    dbUrl += `:${process.env.DB_PORT}`;
   }
-  dbUrl += `/${process.env.DB_NAME}`
+  dbUrl += `/${process.env.DB_NAME}`;
 
-  dbConfig['dbUrl']=dbUrl
+  dbConfig['dbUrl'] = dbUrl;
 
-  return dbConfig
+  return dbConfig;
 }
